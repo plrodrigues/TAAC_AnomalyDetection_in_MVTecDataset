e6329e981017f6a5061ff860da1983277bb50b5f
Felipe <fcoelho.mrc@gmail.com>
2022-11-28 14:15:08 +0000

    Created custom dataset for ViT training

--- src/commons/dataset.py
+++ src/commons/dataset.py
@@ -107,6 +107,73 @@ class MVTECTestDataset(Dataset):
 
         return test_data_img, ground_truth, class_and_id
 
+
+class MVTECViTDataset(Dataset):
+
+    def __init__(self, main_path, category):
+        self.path = os.path.join(main_path, category)
+
+        self.data, self.ground_truth, self.class_and_id, self.weak_labels = self.load_data()
+        
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return {"test": self.data[idx],
+                "ground_truth": self.ground_truth[idx],
+                "class_and_id": self.class_and_id[idx],
+                "labels": self.weak_labels[idx]}
+
+    def load_data(self):
+
+        class_and_id = []
+
+        ## Load data from /train folder
+        train_data_img = []
+        train_path = os.path.join(self.path, "train") 
+        for dir in os.listdir(train_path):
+            for name in glob.glob(os.path.join(train_path, dir) + DIR_SEP + "*.png"):
+                image = read_image(name, mode=ImageReadMode.RGB)
+                train_data_img.append(image)
+                class_and_id.append(name.replace(train_path + DIR_SEP, '').replace('.png', ''))
+
+        ## Load data from /test folder
+        test_data_img = []
+        test_path = os.path.join(self.path, "test") 
+
+        # test images
+        for dir in os.listdir(test_path): # for all test categories (crack, poke, scratch, ...), including good
+            for name in glob.glob(os.path.join(test_path, dir) + DIR_SEP + "*.png"):
+                image = read_image(name, mode=ImageReadMode.RGB)
+                test_data_img.append(image)
+                class_and_id.append(name.replace(test_path + DIR_SEP, '').replace('.png', ''))
+
+        logging.debug(f"{len(class_and_id)} test images loaded")
+        
+        # ground truth masks
+        mask_path = os.path.join(self.path, "ground_truth")
+        logging.debug(f"Loading {len(class_and_id)} masks, from {mask_path} dir")
+
+        weak_labels = []
+        ground_truth = []
+        data_img = train_data_img + test_data_img
+        for img_class_id in class_and_id:
+            mask_name = os.path.join(mask_path, img_class_id) + "_mask.png"
+            logging.debug(f'Loading respective masks: {mask_name}')
+            if 'good' in mask_name: 
+                logging.debug(f'{img_class_id}, good')
+                image = torch.zeros(test_data_img[0].shape)
+                weak_labels.append(1)
+            else: 
+                logging.debug(f'{img_class_id}, not good')
+                image = read_image(mask_name, mode=ImageReadMode.RGB)
+                weak_labels.append(0)
+            ground_truth.append(image)
+
+        return data_img, ground_truth, class_and_id, weak_labels
+
+
+
 class TinyImageNetDataset(Dataset):
     def __init__(self, root_dir, transforms=None):
         '''
@@ -182,8 +249,47 @@ class TinyImageNetDataset(Dataset):
             classes += [index]*len(names)
         return files, classes
 
-# Testing for TinyImageNet
+
+
+# Testing for custom MVTec for weakly-supervised training
 if __name__ == "__main__":
+    # Define the logging level
+    logging.getLogger().setLevel(logging.INFO)
+
+    import matplotlib.pyplot as plt
+    cat = "capsule"
+    test_dataset = MVTECViTDataset(DATA_PATH, cat)
+
+    print(len(test_dataset), len(test_dataset.data), len(test_dataset.ground_truth), len(test_dataset.class_and_id))
+    sample = test_dataset[219 + 3]
+    img, mask, ex_name, labels = sample["test"], sample["ground_truth"], sample['class_and_id'], sample["labels"]
+    
+    fig, (ax1, ax2) = plt.subplots(1,2, figsize=(10,5), sharex=True, sharey=True)
+    ax1.imshow(torch.permute(img, (1, 2, 0)))
+    ax1.set_title(f'Raw image - label {labels}')
+    ax2.imshow(torch.permute(img, (1, 2, 0)))
+    ax2.imshow(torch.permute(mask, (1, 2, 0)), cmap="spring", alpha=0.5, vmax=mask.max()/2)
+    ax2.set_title('Ground truth mask')
+    fig.suptitle(ex_name)
+    plt.show()
+
+    print(img.max(), mask.max())
+
+    sample = test_dataset[50]
+    img, mask, ex_name, labels = sample["test"], sample["ground_truth"], sample['class_and_id'], sample["labels"]
+    
+    fig, (ax1, ax2) = plt.subplots(1,2, figsize=(10,5), sharex=True, sharey=True)
+    ax1.imshow(torch.permute(img, (1, 2, 0)))
+    ax1.set_title(f'Raw image - label {labels}')
+    ax2.imshow(torch.permute(img, (1, 2, 0)))
+    ax2.imshow(torch.permute(mask, (1, 2, 0)), cmap="spring", alpha=0.5, vmax=mask.max()/2)
+    ax2.set_title('Ground truth mask')
+    fig.suptitle(ex_name)
+    plt.show()
+
+
+# Testing for TinyImageNet
+if False:
 
     # Define the logging level
     logging.getLogger().setLevel(logging.DEBUG)
@@ -208,7 +314,7 @@ if __name__ == "__main__":
     plt.title(f"{tiny_dataset.index_to_name[label]}, size: {img.shape}")
     plt.show()
 
-# Testing for other datasaets
+# Testing for basic MVTec data
 if False:
     # extract_tar(TARFILE_PATH, DATA_PATH)
     import matplotlib.pyplot as plt
